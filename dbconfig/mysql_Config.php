2302cb6f58ed9f7cc67aa8d41b2ea6de7751cca5
SOMESH <40345723+someshk97@users.noreply.github.com>
2022-06-24 20:43:37 +0530

    seconds 3 files

---
+++ dbconfig/mysql_Config.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 	Database config variables	 */
+define("ORA_CON_DBHOSTNAME", "localhost");
+define("ORA_CON_USER", "root");             // username
+define("ORA_CON_PASSWORD", "");             // password
+define("ORA_CON_DATABASE", "oas" ); 		// connection string host and service:localhost
+
+/*	Tablenames Variables	*/
+define("USER_APPLICATION_DETAILS_TABLE",   "T_USER"); 
+define("USER_PERSONAL_DETAILS_TABLE",      "T_USERDATA"); 	
+
+
+?>
---
+++ email/config.php
@@ -0,0 +1,12 @@
+<?php
+
+//SENDER EMAIL CONFIGURATIONS
+define("EMAIL_SERVERNAME",   "smtp.gmail.com"); 	
+define("EMAIL_USERNAME",    "admissions.mtech@iipe.ac.in");
+define("EMAIL_PASSWORD_TEMP", 	  "xxvihoykuxcyycjw"); 
+define("EMAIL_FROM", 	  "admissions.mtech@iipe.ac.in");
+define("EMAIL_FROMNAME", 	  "IIPE M.TECH ADMISSIONS");
+
+define("EMAIL_IN_CC", 	  "itosystem@iipe.ac.in");
+define("USERNAME_IN_CC", 	  "IT OFFICER SYSTEMS");
+?>
