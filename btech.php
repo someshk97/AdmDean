54a3bac27cbc76b8c7d63ac1e42a0d4b661d1e1e
SOMESH <40345723+someshk97@users.noreply.github.com>
2022-06-24 20:47:54 +0530

    fourth commit

---
+++ btech.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link type="text/css" rel="stylesheet" href="css/login.css">
+    </link>
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap/bootstrap-theme.min.css">
+    <script src="bootstrap/jquery.min.js"></script>
+    <script src="bootstrap/bootstrap.min.js"></script>
+    <script src="js/validate_index.js"></script>
+
+    <title>B.Tech. Admissions</title>
+</head>
+
+<body bgcolor="white">
+        <table width=100% border="0">
+            <tr>
+                <td align="center" width=18% align="center"><a href=""><img src="images/Logo.jpg" width=130 border="0">
+                        <img src="images/english.jpg" width=400 alt="Academics Homepage"><br>
+                        <img src="images/tag.jpg" width=400></td>
+            </tr>
+        </table>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-12">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<h4 align="center"><strong>APPLICATION SCRUTINY PORTAL</strong></h4>
+                </div>
+            </div>
+        </div> 
+</body>
+
+</html>
